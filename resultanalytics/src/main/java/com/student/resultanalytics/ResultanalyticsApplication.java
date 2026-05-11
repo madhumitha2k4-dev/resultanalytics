464c851b902d8ddf19b5007dac2f43f62ca3a6f4
madhumitha2k4-dev <madhumitha2k4@gmail.com>
2026-05-11 19:57:16 +0530

    Initial Spring Boot project setup

---
+++ resultanalytics/src/main/java/com/student/resultanalytics/ResultanalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.student.resultanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultanalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultanalyticsApplication.class, args);
+	}
+
+}
---
+++ resultanalytics/src/test/java/com/student/resultanalytics/ResultanalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.resultanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultanalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
