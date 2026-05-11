303aa6bb42aab8eff1a93139c1a94a5c4d09452b
madhumitha2k4-dev <madhumitha2k4@gmail.com>
2026-05-11 22:54:07 +0530

    Added Student controller and POST API

---
+++ resultanalytics/src/main/java/com/student/resultanalytics/controller/StudentController.java
@@ -0,0 +1,19 @@
+package com.student.resultanalytics.controller;
+
+import com.student.resultanalytics.entity.Student;
+import com.student.resultanalytics.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.saveStudent(student);
+    }
+}
