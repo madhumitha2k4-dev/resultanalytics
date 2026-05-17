0f37ae499ef6c397a3ad0cb6709c6ebed60384a0
madhumitha2k4-dev <madhumitha2k4@gmail.com>
2026-05-17 10:57:05 +0530

    Added analytics APIs in StudentController

--- resultanalytics/src/main/java/com/student/resultanalytics/controller/StudentController.java
+++ resultanalytics/src/main/java/com/student/resultanalytics/controller/StudentController.java
@@ -35,6 +35,24 @@ public class StudentController {
         return "Student deleted successfully";
     }
 
+    @PutMapping("/{id}")
+public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
+
+    Student existingStudent = studentService.getStudentById(id);
+
+    existingStudent.setName(student.getName());
+    existingStudent.setUsn(student.getUsn());
+    existingStudent.setDepartment(student.getDepartment());
+    existingStudent.setSemester(student.getSemester());
+    existingStudent.setMarks1(student.getMarks1());
+    existingStudent.setMarks2(student.getMarks2());
+    existingStudent.setMarks3(student.getMarks3());
+    existingStudent.setAverage(student.getAverage());
+    existingStudent.setSgpa(student.getSgpa());
+    existingStudent.setEmail(student.getEmail());
+
+    return studentService.saveStudent(existingStudent);
+}
     @GetMapping("/topper")
 public Student getTopper() {
     return studentService.getTopper();
